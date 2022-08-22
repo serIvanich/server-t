3c3078b3418d9478b11dd7c325ebfa75e03ab7e1
serIvanich <serivan1108@gmail.com>
2022-08-22 21:29:23 +0300

    added express and created listener

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/samurais', (req, res) => {
+  res.send('Hello samurais!')
+})
+
+app.post('/samurais', (req, res) => {
+  res.send('Add some samurais')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
