3272dffa2d720c871fcb0807fdb0dae28468ceef
serIvanich <serivan1108@gmail.com>
2022-08-24 17:31:54 +0300

    added nodemon

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
 
-app.get('/samurais', (req, res) => {
-  res.send('Hello samurais!')
-})
+app.get("/samurais", (req, res) => {
+  res.send("Hello samurais!");
+});
 
-app.post('/samurais', (req, res) => {
-  res.send('Add some samurais')
-})
+app.post("/samurais", (req, res) => {
+  res.send("Add some samurais");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
